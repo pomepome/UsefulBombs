d8a86830c8658980e98f5cebc56dd77df22b3cc2
pomepome <punyoon03@gmail.com>
2018-07-09 01:59:17 +0900

    1.0

---
+++ UsefulBombs/ModConfig.cs
@@ -0,0 +1,10 @@
+﻿namespace UsefulBombs
+{
+    internal class ModConfig
+    {
+        public bool ModifyDamagesToEnemies = true;
+        public bool BreakBoulders = true;
+        public bool CollectCrystals = false;
+        public float DamageMultiplier = 3.0f;
+    }
+}
